fb7022c9fa84d3459286c3cbeef291d79bf5d0d4
İmranov <semtexpert@gmail.com>
2023-12-27 15:47:08 +0300

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Kullanıcıdan kaç tane sayı gireceğini al
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        int n = scanner.nextInt();
+
+        // Eğer hiç sayı girmeyecekse kullanıcıya bilgi ver ve programı sonlandır
+        if (n <= 0) {
+            System.out.println("Lütfen geçerli bir sayı girin.");
+            return;
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int i = 1;
+
+        // Girilen sayıları kontrol et ve en büyük ile en küçüğü güncelle
+        while (i <= n) {
+            System.out.print(i + ". Sayıyı giriniz: ");
+            int number = scanner.nextInt();
+
+            if (number > max) {
+                max = number;
+            }
+
+            if (number < min) {
+                min = number;
+            }
+
+            i++;
+        }
+
+        // En büyük ve en küçük sayıları ekrana yazdır
+        System.out.println("En büyük sayı: " + max);
+        System.out.println("En küçük sayı: " + min);
     }
 }
